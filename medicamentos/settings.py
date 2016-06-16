859530fbb716c89c2aa4aa6d193166c4486e2bab
Carlos <activo23.kt@gmail.com>
2016-06-16 14:03:29 -0600

    django-chartjs agregador

--- medicamentos/settings.py
+++ medicamentos/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chartjs',
     'variacionMedicamentos.apps.VariacionmedicamentosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
