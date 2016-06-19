a9c7a3ec14533fc73ad25e6ffd6e6c33c33c6ec6
root <root@localhost.localdomain>
2016-06-19 03:10:41 -0500

    Consulta Por farmacia agregado, adminitrador personalizado

--- variacionMedicamentos/admin.py
+++ variacionMedicamentos/admin.py
@@ -10,19 +10,29 @@ class ConcentracionesAdmin(admin.ModelAdmin):
     inlines = [
         Med_ConcenInline,
     ]
+    list_display = ('id', 'valor')
 
 
 class MedAdmin(admin.ModelAdmin):
-    exclude = ('concentraciones',)
-
     inlines = [
         Med_ConcenInline,
     ]
     model = Medicamento
+    list_display = ('codigo_medicamento', 'nombre_medicamento')
+
+class Med_FarInline(admin.TabularInline):
+    model = Farmacia.medicamentos.through
+
+class FarmaciaAdmin(admin.ModelAdmin):
+
+    inlines = [
+        Med_FarInline,
+    ]
+    list_display = ('nombre_farmacia', 'descuento')
 
 
 admin.site.register(Medicamento, MedAdmin)
 admin.site.register(Med_Concentracion)
 admin.site.register(Concentracion, ConcentracionesAdmin)
-admin.site.register(Farmacia)
+admin.site.register(Farmacia, FarmaciaAdmin)
 # Register your models here.
